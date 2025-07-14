6415803a4f262df10e82401091ebac1cc6dc9d30
Joel Dice <joel.dice@fermyon.com>
2025-07-14 13:41:16 -0600

    initial commit
    Signed-off-by: Joel Dice <joel.dice@fermyon.com>

---
+++ Makefile
@@ -0,0 +1,7 @@
+.PHONY: build
+build: wasi_snapshot_preview1.proxy.wasm
+	cargo build --release --target wasm32-wasip1
+	wasm-tools component new --skip-validation target/wasm32-wasip1/release/hello_wasip3_http.wasm --adapt wasi_snapshot_preview1.proxy.wasm -o hello.wasm
+
+wasi_snapshot_preview1.proxy.wasm:
+	curl -OL https://github.com/bytecodealliance/wasmtime/releases/download/v34.0.1/wasi_snapshot_preview1.proxy.wasm
---
+++ src/lib.rs
@@ -0,0 +1,35 @@
+use {
+    crate::{
+        exports::wasi::http::handler::Guest,
+        wasi::http::types::{ErrorCode, Fields, Request, Response},
+    },
+    wit_bindgen_rt::async_support,
+};
+
+wit_bindgen::generate!({
+    path: "wit",
+    world: "wasi:http/proxy@0.3.0-draft",
+    generate_all,
+    debug: true,
+});
+
+struct Component;
+
+export!(Component);
+
+impl Guest for Component {
+    async fn handle(_request: Request) -> Result<Response, ErrorCode> {
+        let (trailers_tx, trailers_rx) = wit_future::new(|| unreachable!());
+        let (mut content_tx, content_rx) = wit_stream::new();
+
+        async_support::spawn(async move {
+            content_tx
+                .write_all(b"Hello, wasi:http/proxy world!\n".to_vec())
+                .await;
+
+            trailers_tx.write(Ok(None)).await.unwrap();
+        });
+
+        Ok(Response::new(Fields::new(), Some(content_rx), trailers_rx).0)
+    }
+}
