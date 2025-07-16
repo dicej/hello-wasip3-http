2c3e8975b0cbefa5181cd5cd961feeeb68094a60
Joel Dice <joel.dice@fermyon.com>
2025-07-16 11:09:53 -0600

    simplify code
    Signed-off-by: Joel Dice <joel.dice@fermyon.com>

--- src/lib.rs
+++ src/lib.rs
@@ -18,15 +18,13 @@ export!(Component);
 
 impl Guest for Component {
     async fn handle(_request: Request) -> Result<Response, ErrorCode> {
-        let (trailers_tx, trailers_rx) = wit_future::new(|| unreachable!());
+        let (_, trailers_rx) = wit_future::new(|| Ok(None));
         let (mut content_tx, content_rx) = wit_stream::new();
 
         async_support::spawn(async move {
             content_tx
                 .write_all(b"Hello, wasi:http/proxy world!\n".to_vec())
                 .await;
-
-            trailers_tx.write(Ok(None)).await.unwrap();
         });
 
         Ok(Response::new(Fields::new(), Some(content_rx), trailers_rx).0)
